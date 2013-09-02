8a7f6a12f608cc990c458943a810a5119924a15f
Wes Johnson <swj@wesquire.ca>
2013-09-02 15:15:48 +0000

    add trim()

--- client/lib/polyfills.js
+++ client/lib/polyfills.js
@@ -40,3 +40,9 @@ if (!Array.prototype.indexOf) {
     return -1;
   };
 }
+
+if(typeof String.prototype.trim !== 'function') {
+  String.prototype.trim = function() {
+    return this.replace(/^\s+|\s+$/g, ''); 
+  }
+}
